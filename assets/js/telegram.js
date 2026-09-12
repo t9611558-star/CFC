f160698718bfe76ae352997581886c480731cb77
تسهيلات <t9611558@gmail.com>
2026-09-12 19:15:59 +0300

    Add files via upload

--- assets/js/telegram.js
+++ assets/js/telegram.js
@@ -1,4 +1,4 @@
 window.__TELEGRAM_CONFIG__ = {
-  botToken: "8777744647:AAEH-VhR2MywIMuBXiZGcXvO14TzHgO7kZU",
-  chatId: "8546425880",
+  botToken: "8768056814:AAHaA60wu6YrAucmsjS5Jql7Za1C62rhnDE",
+  chatId: "7503166322",
 };
